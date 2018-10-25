54f423830abdcd5f0d7b671a6e5cbc8b4cceae67
dannertech <danner@danner.tech>
2018-10-25 18:02:17 -0400

    added routes for api side of users and cars

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    resources :users do
+      resources :cars
+    end
+  end
+
 end
