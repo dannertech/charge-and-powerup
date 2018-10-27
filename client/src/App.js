cfaaa0f6990191cd265b214054ae3d704a62b570
dannertech <danner@danner.tech>
2018-10-26 23:13:34 -0400

    added routes for all users, single user, and landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
-
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import LandingPage from './components/LandingPage'
+import AllUsers from './components/AllUsers'
+import SingleUser from './components/SingleUser'
 
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-  
+  <Router>
+    <Switch>
+      <Route exact path='/' component={LandingPage} />
+      <Route exact path='/users' component={AllUsers} />
+      <Route exact path='/users/:id' component={SingleUser} />
+
+    </Switch>
+  </Router>
     );
   }
 }
