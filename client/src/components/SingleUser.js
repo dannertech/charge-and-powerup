c4d72f5023532a587c9c2409b10f31e32a2da80f
dannertech <danner@danner.tech>
2018-10-31 00:26:01 -0400

    edited SingleUser page to fetch user data and car data

--- client/src/components/SingleUser.js
+++ client/src/components/SingleUser.js
@@ -3,6 +3,28 @@ import { Link } from 'react-router-dom'
 import axios from 'axios'
 
 export default class SingleUser extends Component {
+    state = {
+        user: {},
+        cars: []
+    }
+
+    async componentDidMount(){
+        const userId = this.props.match.params.id
+        const user = await this.fetchUser(userId)
+        const cars = await this.fetchCars(userId)
+        this.setState({ user,cars})
+
+    }
+
+    fetchUser = async(id) => {
+const response = await axios.get(`/api/users/${id}`)
+this.setState({user: response.data})
+    }
+
+    fetchCars = async(id) => {
+        const response = await axios.get(`/api/users/${id}`)
+        this.setState({cars: response.data})
+    }
     render() {
         return (
             <h1>Hello as well</h1>
