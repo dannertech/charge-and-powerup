eb9b9d20d4079413319628acad1fbfa4ac78bdd9
dannertech <danner@danner.tech>
2018-11-01 10:39:40 -0400

    edited handleChange function

--- client/src/components/AccountSettings.js
+++ client/src/components/AccountSettings.js
@@ -3,10 +3,30 @@ import axios from 'axios'
 import styled from 'styled-components'
 
 export default class AccountSettings extends Component {
+    state = {
+        user: {}
+    }
+    handleChange = (event) => {
+        const updatedUser = {...this.state.user}
+        updatedUser[event.target.name] = event.target.value
+        this.setState({user:updatedUser})
+    }
+
+    handleSubmit = (event,updatedUser) => {
+        event.preventDefault()
+        const currentUser = {...this.state.user}
+        currentUser.username = updatedUser.username
+       
+
+ 
+    }
+
+
 render(){
     return(
         <div>
             <h1>Settings Page</h1>
+
         </div>
     )
 }
