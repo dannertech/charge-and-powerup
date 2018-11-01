f9221ac069dbaf1f92e2b821aba35bf8a006314f
dannertech <danner@danner.tech>
2018-11-01 11:53:14 -0400

    updated signup page to include form and updated state

--- client/src/components/SignupPage.js
+++ client/src/components/SignupPage.js
@@ -4,10 +4,26 @@ import styled from 'styled-components'
 
 
 export default class SignupPage extends Component {
+    state = {
+        users: [],
+        newUser: {
+            password: '',
+            username: ''
+        }
+    }
     render(){
         return(
             <div>
                 <h1>This is the Signup Page</h1>
+                <form onSubmit={this.handleSubmit}>
+                <label> Username </label>
+                <br></br>
+                <input type="text" name="username" value={this.state.newUser.username} onChange={this.handleChange} />
+                <br></br>
+                <label>Password</label>
+                <br></br>
+                <input type="text" name="password" value={this.state.newUser.password} onClick={this.handleChange} />
+                </form>
             </div>
         )
     }
