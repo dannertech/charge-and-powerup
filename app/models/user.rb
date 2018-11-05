ffcb6c318ff1300840056a2aeb7e1033507ed5b4
dannertech <danner@danner.tech>
2018-11-05 18:04:29 -0500

    updated validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     has_many :cars, dependent: :destroy
-    validates :username, presence: true, uniqueness: { this username is already connected to an account }
-    validates :email, presence: true, uniqueness: { this email is already connected to an account }
+    validates :username, presence: true, uniqueness: { message: this username is already connected to an account }
+    validates :email, presence: true, uniqueness: { message: this email is already connected to an account }
 end
