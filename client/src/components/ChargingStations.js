4cd7594da6805ec1f56ee0987e98f784256bcbfd
dannertech <danner@danner.tech>
2018-11-11 14:28:11 -0500

    added form to chargingstations component

--- client/src/components/ChargingStations.js
+++ client/src/components/ChargingStations.js
@@ -6,15 +6,24 @@ export default class ChargingStations extends Component {
     state = {
         stations: [],
         filteredStations: [],
-        citySearch: ''
+        citySearch: {
+            userInput: ''
+        }
     }
 
 async componentDidMount(){
     this.getStations()
-  
+}
+
+handleSubmit = (event) => {
+    event.preventDefault()
 
 }
 
+handleChange = (event) => {
+const userInput = {...this.state.stations}
+userInput[event.target.name] = event.target.value
+}
 getStations = async() => {
     const response = await axios.get('https://api.openchargemap.io/v2/poi')
     console.log(response)
@@ -25,8 +34,23 @@ getStations = async() => {
     render() {
 
         return (
+            <div>
             <h1>This is the charging stations page</h1>
-
+            <div class="row">
+    <form class="col s12">
+      <div class="row">
+        <div class="col s12">
+          Search for Stations:
+          <div class="input-field inline">
+            <input name="userInput" id="email_inline" type="email" class="validate"></input>
+            <label for="email_inline">City Name</label>
+            <button type="submit">Submit</button>
+          </div>
+        </div>
+      </div>
+    </form>
+  </div>
+  </div>
         )
     }
 }
