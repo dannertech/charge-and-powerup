496b599046dbded945946e7fe54ceae3949a4f28
dannertech <danner@danner.tech>
2018-11-11 14:14:14 -0500

    added axios call to get stations

--- client/src/components/ChargingStations.js
+++ client/src/components/ChargingStations.js
@@ -3,9 +3,30 @@ import axios from 'axios'
 import styled from 'styled-components'
 
 export default class ChargingStations extends Component {
+    state = {
+        stations: [],
+        filteredStations: [],
+        citySearch: ''
+    }
+
+async componentDidMount(){
+    this.getStations()
+  
+
+}
+
+getStations = async() => {
+    const response = await axios.get('https://api.openchargemap.io/v2/poi')
+    console.log(response)
+    this.setState({ stations: response.data}) 
+    
+}
+
     render() {
+
         return (
             <h1>This is the charging stations page</h1>
+
         )
     }
 }
